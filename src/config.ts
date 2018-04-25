d79ffb506e4875267e52d0af9b95463395105056
Kyle Pfromer <kpfromer2@gmail.com>
2018-04-25 10:43:13 -0600

    Add config

---
+++ src/config.ts
@@ -0,0 +1,31 @@
+import * as Confidence from 'confidence';
+
+const constants = {
+  APP_TITLE: 'Smarttodo',
+  CLIENT_URL: 'https://smarttodo.com',
+  PORT: 3000
+};
+
+const config = {
+  port: {
+    $filter: 'env',
+    production: process.env.PORT,
+    $default: constants.PORT
+  }
+};
+
+const criteria = {
+  env: process.env.NODE_ENV
+};
+
+const store = new Confidence.Store(config);
+
+export const Config = new class {
+  get(key: string) {
+    return store.get(key, criteria);
+  }
+
+  getMetadata(key: string) {
+    return store.meta(key, criteria);
+  }
+}();
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from 'app.module';
+import { Config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(Config.get('/port'));
 }
 bootstrap();
