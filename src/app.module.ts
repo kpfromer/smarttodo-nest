879a2ac81283e6edbe172a850f1b0b627f9b4d7f
Kyle Pfromer <kpfromer2@gmail.com>
2018-04-25 09:23:36 -0600

    Add nest-router

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { RouterModule } from 'nest-router';
+import { ROUTES } from './routes';
 
 @Module({
   imports: [
-    TypegooseModule.forRoot('mongodb://localhost/smarttodo-nest')
+    TypegooseModule.forRoot('mongodb://localhost/smarttodo-nest'),
+    RouterModule.forRoutes(ROUTES)
   ]
 })
 export class AppModule {}
---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from 'nest-router';
+
+export const ROUTES: Routes = [
+  {
+    path: 'v1',
+    children: []
+  }
+];
