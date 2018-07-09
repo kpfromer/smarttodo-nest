71895beef64099aca71d2cd7789f90efdd5a6d1c
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-08 19:10:48 -0600

    Add login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from 'nest-router';
+import { LoginModule } from './modules/login/login.module';
 import { RegisterModule } from './modules/register/register.module';
+import { TodoModule } from './modules/todo/todo.module';
 
 export const ROUTES: Routes = [
   {
     path: '/v1',
     children: [
+      {
+        path: '/login',
+        module: LoginModule
+      },
       {
         path: '/register',
         module: RegisterModule
