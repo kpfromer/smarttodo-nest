9ecc423422c15a521282a1887da777d742966797
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-08 19:14:50 -0600

    Fix unfound Todo

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from 'nest-router';
 import { LoginModule } from './modules/login/login.module';
 import { RegisterModule } from './modules/register/register.module';
-import { TodoModule } from './modules/todo/todo.module';
+
 
 export const ROUTES: Routes = [
   {
